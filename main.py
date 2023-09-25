fc578d19205bd1563acfacae413a29b3f5a62011
null <null>
2023-09-25 17:18:58 +0000

    test

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ hash = pbkdf2_hmac('sha256', password, b'D8VxSmTZt2E2YV454mkqAY5e', 100000)    #
 
 # normal code
 custom_message = 'Tu wpisz co chcesz :)'
-message = os.getenv('APPENV', 'Default Hello World!')
-message2 = 'Sample-app - Wersja: 1.47.\nZmienna APPENV: ' + message + '\n' custom_message
+message = os.getenv('APPENV', 'Default Hello World!\n')
+message2 = 'Sample-app - Wersja: 1.47.\nZmienna APPENV: ' + message + custom_message
 
 class handler(BaseHTTPRequestHandler):
 
